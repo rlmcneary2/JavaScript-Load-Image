9b66f791efb099cb4e1fd460bc8f5f5c021ee379
Rich McNeary <rlmcneary2@gmail.com>
2016-07-08 17:01:17 -0400

    Though mapping is not really needed added a getAll() function for compatibility.

---
+++ js/load-image-xmp-map.js
@@ -0,0 +1,27 @@
+;(function (factory) {
+  'use strict'
+  if (typeof define === 'function' && define.amd) {
+    // Register as an anonymous AMD module:
+    define(['./load-image', './load-image-exif'], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    factory(require('./load-image'), require('./load-image-exif'))
+  } else {
+    // Browser globals:
+    factory(window.loadImage)
+  }
+}(function (loadImage) {
+  'use strict'
+
+  loadImage.XmpMap.prototype.getAll = function () {
+    var map = {};
+    var prop;
+    var id;
+    for (prop in this) {
+      if (this.hasOwnProperty(prop)) {
+          map[prop] = this[prop];
+      }
+    }
+    return map;
+  };
+
+}));
